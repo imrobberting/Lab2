6fc3b15c00a17ffa36687b59f7e5d14da0971268
tsdmgz <tsdmgz@gmail.com>
2015-11-01 15:04:52 +0800

    Make server side validation more robust

--- submit.php
+++ submit.php
@@ -19,14 +19,31 @@
 		$invO = substr($contents[1], 25);
 		$invB = substr($contents[2], 25);
 
+		$name = $_POST['customer'];
+		$oranges = $_POST['oranges'];
+		$apples = $_POST['apples'];
+		$bananas = $_POST['bananas'];
+		$card = $_POST['mode'];
+
 		//simpler implementation, form checking code http://stackoverflow.com/a/14251049
 		$fields = array('customer','oranges','apples','bananas','mode');
+		$int_fields = array('oranges','apples','bananas');
 		$error = false;
 		foreach($fields AS $fieldname) {
 			if(!isset($_POST[$fieldname]) || empty($_POST[$fieldname])) {
 					echo "An entry was not filled out. Please check and try again.<br/>" . '<a href="javascript:history.go(-1)">Click here to go back.</a>';
 					//it's not friendly, but does the job
 					die();
+			}
+		}
+
+		foreach($int_fields AS $number) {
+			if(!is_numeric($_POST[$number]) || $_POST[$number] < 0) { ?>
+				<p>Non numerical numbers are not a number, unless this is algebra. But we're not working with algebra. Negative fruits are also impossible (we don't do refunds, if that's what you mean). The Laws of Physics simply cannot allow that.</p>
+				<p>Have a nice day. <a href="javascript:history.go(-1)">Try again?</a></p>
+				<blockquote class="twitter-tweet" lang="en"><p lang="nl" dir="ltr">QA Engineer walks into a bar. Orders a beer. Orders 0 beers. Orders 999999999 beers. Orders a lizard. Orders -1 beers. Orders a sfdeljknesv.</p>&mdash; Bill Sempf (@sempf) <a href="https://twitter.com/sempf/status/514473420277694465">September 23, 2014</a></blockquote>
+<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+				<?php die();
 			}
 		}
 
